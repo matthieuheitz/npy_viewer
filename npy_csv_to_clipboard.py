cf128689bba6f0adf40b0c530eb58eaaa3784668
Matthieu Heitz <heitz.matthieu@gmail.com>
2018-04-13 15:39:31 +0200

    Add a script for copying array to clipboard as a CSV

---
+++ npy_csv_to_clipboard.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+'''
+This script copies the content of the NPY file
+in the clipboard in a CSV.
+This is intended to be pasted into a spreadsheet program.
+
+If multiple files are selected, they will all be copied one
+below the other, with a blank line separating them.
+'''
+
+import numpy as np
+import sys
+import io
+import pyperclip
+import csv
+
+s = io.StringIO()
+csvWriter = csv.writer(s)
+
+for i in range(1,len(sys.argv)):
+    file = sys.argv[i]
+    A = np.load(file)
+    csvWriter.writerows(A)
+    if i != len(sys.argv):
+        csvWriter.writerows(' ')
+pyperclip.copy(s.getvalue())
+
+input("Hit Enter to quit ...")
+
