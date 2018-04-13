8d5614fa7670e3ac6478eb48ff28b09ea91b51db
Matthieu Heitz <heitz.matthieu@gmail.com>
2018-04-13 15:02:43 +0200

    Switch to Python 3

--- npy_file_compare.py
+++ npy_file_compare.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,7 +6,7 @@ import sys
 import os
 
 if len(sys.argv) != 3:
-    print "ERROR: Exactly two files must be selected for comparison"
+    print("ERROR: Exactly two files must be selected for comparison")
     input("Hit Enter to quit ...")
     exit(1)
 
@@ -15,52 +15,52 @@ f2 = sys.argv[2]
 A1 = np.load(f1)
 A2 = np.load(f2)
 
-print "Comparing the 2 files:\n"
-print "f1: ",os.path.basename(f1),"\n"
-print A1
-print "\n"
-print "f2: ",os.path.basename(f2),"\n"
-print A2
-print "\n"
+print("Comparing the 2 files:\n")
+print("f1: ",os.path.basename(f1),"\n")
+print(A1)
+print("\n")
+print("f2: ",os.path.basename(f2),"\n")
+print(A2)
+print("\n")
 
 if A1.shape != A2.shape:
-    print "The two files have different shapes:"
-    print os.path.basename(f1)," has shape ",A1.shape
-    print os.path.basename(f2)," has shape ",A2.shape
-    print "The two files must be of same shape for further comparison."
+    print("The two files have different shapes:")
+    print(os.path.basename(f1)," has shape ",A1.shape)
+    print(os.path.basename(f2)," has shape ",A2.shape)
+    print("The two files must be of same shape for further comparison.")
     input("Hit Enter to quit ...")
     exit(1)
 
 abs_err = np.abs(A2-A1)
 if np.sum(abs_err != 0) == 0:
-    print "The two files are identical.\n"
+    print("The two files are identical.\n")
     input("Hit Enter to quit ...")
     exit(1)
 
-print "The two files have the same shape: ",A1.shape,"\n"
+print("The two files have the same shape: ",A1.shape,"\n")
 
-print "Distance functions :"
-print "0-norm = ",np.sum(abs_err != 0)
-print "Total Variation = ",np.sum(abs_err)
-print "Quadratic Loss = ",np.sum((A2-A1)**2)
-print "KL(A1|A2) = ",np.sum(A1*np.log(A1/A2)-A1+A2)
-print "KL(A2|A1) = ",np.sum(A2*np.log(A2/A1)-A2+A1)
-print "\n"
+print("Distance functions :")
+print("0-norm = ",np.sum(abs_err != 0))
+print("Total Variation = ",np.sum(abs_err))
+print("Quadratic Loss = ",np.sum((A2-A1)**2))
+print("KL(A1|A2) = ",np.sum(A1*np.log(A1/A2)-A1+A2))
+print("KL(A2|A1) = ",np.sum(A2*np.log(A2/A1)-A2+A1))
+print("\n")
 
-print "Stats on the absolute difference : \n"
-print "min \t=",np.min(abs_err)
-print "max \t=",np.max(abs_err)
-print "mean \t=",np.mean(abs_err)
-print "median \t=",np.median(abs_err)
-print "std_dev\t=",np.std(abs_err)
-print "\n"
+print("Stats on the absolute difference : \n")
+print("min \t=",np.min(abs_err))
+print("max \t=",np.max(abs_err))
+print("mean \t=",np.mean(abs_err))
+print("median \t=",np.median(abs_err))
+print("std_dev\t=",np.std(abs_err))
+print("\n")
 
-print "Number of equal values: ",np.sum(abs_err == 0)
-print "Stats on the absolute difference without equal values: \n"
-print "min \t=",np.min(abs_err[abs_err > 0])
-print "mean \t=",np.mean(abs_err[abs_err > 0])
-print "std_dev\t=",np.std(abs_err[abs_err > 0])
-print "\n"
+print("Number of equal values: ",np.sum(abs_err == 0))
+print("Stats on the absolute difference without equal values: \n")
+print("min \t=",np.min(abs_err[abs_err > 0]))
+print("mean \t=",np.mean(abs_err[abs_err > 0]))
+print("std_dev\t=",np.std(abs_err[abs_err > 0]))
+print("\n")
 
 if len(A1.shape) == 2:
     plt.figure(figsize=(8,8))
--- npy_file_viewer.py
+++ npy_file_viewer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import numpy as np
 import sys
@@ -7,15 +7,15 @@ import os
 for i in range(1,len(sys.argv)):
     file = sys.argv[i]
     A = np.load(file)
-    print os.path.basename(file)," :\n"
-    print A
-    print "shape \t=",A.shape
-    print "min \t=",np.min(A)
-    print "max \t=",np.max(A)
-    print "abs_min\t=",np.min(np.abs(A))
-    print "mean \t=",np.mean(A)
-    print "median \t=",np.median(A)
-    print "std_dev\t=",np.std(A)
-    print "\n"
+    print(os.path.basename(file)," :\n")
+    print(A)
+    print("shape \t=",A.shape)
+    print("min \t=",np.min(A))
+    print("max \t=",np.max(A))
+    print("abs_min\t=",np.min(np.abs(A)))
+    print("mean \t=",np.mean(A))
+    print("median \t=",np.median(A))
+    print("std_dev\t=",np.std(A))
+    print("\n")
 
 input("Hit Enter to quit ...")
