57b1ad7676da636fb2cd7fa0c76596418668b843
Matthieu Heitz <heitz.matthieu@gmail.com>
2021-10-15 17:53:01 -0700

    Add support for object arrays

--- npy_file_viewer.py
+++ npy_file_viewer.py
@@ -6,6 +6,7 @@ This script gives statistics about the array in the NPY file
 
 import os
 import sys
+
 print("Python version:",sys.version)
 print("Python interpreter:",sys.executable)
 try:
@@ -19,16 +20,27 @@ except ImportError as error:
 
 for i in range(1,len(sys.argv)):
     file = sys.argv[i]
-    A = np.load(file)
+    # I know this is bad for security, but it's necessary for viewing object arrays.
+    A = np.load(file, allow_pickle=True)
     print(os.path.basename(file)," :\n")
     print(A)
-    print("shape \t=",A.shape)
-    print("min \t=",np.min(A))
-    print("max \t=",np.max(A))
-    print("abs_min\t=",np.min(np.abs(A)))
-    print("mean \t=",np.mean(A))
-    print("median \t=",np.median(A))
-    print("std_dev\t=",np.std(A))
+    print("\nshape \t=",A.shape)
+    A[2] = "hello"*250
+    if A.dtype == np.dtype("O"):
+        print("A is an 'object' array:")
+        for i,e in enumerate(A):
+            print("i=%d: type:"%i,type(e),end="")
+            try: print(", len = %d"%len(e),end="")     # If e is iterable
+            except: pass
+            if isinstance(e, str): print(": %s"%e[:100], "..." if len(e)>100 else "", end="")  # If e is a str, print the beginning
+            print()
+    else:
+        print("min \t=",np.min(A))
+        print("max \t=",np.max(A))
+        print("abs_min\t=",np.min(np.abs(A)))
+        print("mean \t=",np.mean(A))
+        print("median \t=",np.median(A))
+        print("std_dev\t=",np.std(A))
     print("\n")
 
 if sys.platform.startswith('linux'):
