7ca809925caf448f4684243c204cb74ba0c76db4
Matthieu Heitz <heitz.matthieu@gmail.com>
2023-02-16 17:32:51 -0800

    Add scripts for H5AD

---
+++ h5ad_viewer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import anndata
+import sys
+
+adata = anndata.read_h5ad(sys.argv[1])
+print("##############################")
+print(adata)
+print("\n##############################\n")
+print("adata.X:\n",adata.X)
+if not adata.obs.empty:
+    print("\n##############################\n")
+    print("adata.obs:\n",adata.obs)
+if not adata.var.empty:
+    print("\n##############################\n")
+    print("adata.var:\n",adata.var)
+
+if sys.platform.startswith('linux'):
+    input("Hit Enter to quit ...")
